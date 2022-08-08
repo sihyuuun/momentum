561cb0f307b59b07e9df1c44adb5eadc935f7b12
shyunshine <dnpsel426@naver.com>
2022-08-08 13:30:00 +0900

    Momentum CSS - 1

--- js/greetings.js
+++ js/greetings.js
@@ -14,7 +14,7 @@ function onLoginSubmit(event) {
 
 function paintGreetings() {
   const username = localStorage.getItem(USERNAME_KEY);
-  greeting.innerText = `hello ${username}`;
+  greeting.innerText = `Hello, ${username} :-)`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
--- js/todo.js
+++ js/todo.js
@@ -23,10 +23,10 @@ function paintToDo(newTodo) {
   const span = document.createElement("span");
   span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.innerText = "❌";
+  button.innerText = "X";
   button.addEventListener("click", deleteToDo);
-  li.appendChild(span);
   li.appendChild(button);
+  li.appendChild(span);
   toDoList.appendChild(li);
 }
 
